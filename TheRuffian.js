9a6f6d844850f3dc6543abb4769536a028907996
Jason Shive <135413+jwshive@users.noreply.github.com>
2021-03-19 07:50:46 -0400

    Moved invite and greeting embed to their own files Setup to know when to use dev and when to run in prod mode

--- TheRuffian.js
+++ TheRuffian.js
@@ -1,15 +1,26 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
-let wowgif =
-  "https://thumbs.gfycat.com/OrganicNastyAmericanriverotter-size_restricted.gif";
+const greetingEmbed = require("./embeds/greeting-embed");
 const { QuickDiscordBot } = require("quick-chat-bot");
 const path = require("path");
+let bot_token;
+let testMode;
+if (process.env.NODE_ENV === "production") {
+  bot_token = process.env.PROD_BOT_TOKEN;
+  testMode = false;
+  console.log("running with the prod token");
+} else {
+  bot_token = process.env.DEV_BOT_TOKEN;
+  testMode = false;
+  console.log("running with the dev token");
+}
+
 const bot = new QuickDiscordBot({
-  botToken: process.env.BOT_TOKEN,
+  botToken: bot_token,
   commandsDir: path.join(__dirname, "commands"),
   ignoreChannels: [],
-  testMode: false,
+  testMode: testMode,
   testChannel: "secret-bot-testing",
   ignoreBots: true,
 });
@@ -17,50 +28,10 @@ const bot = new QuickDiscordBot({
 bot.connect();
 
 bot.client.on("ready", () => {
-  bot.client.user.setActivity("Dreams Shatter", {type: "WATCHING"});
+  bot.client.user.setActivity("Dreams Shatter", { type: "WATCHING" });
 });
 
 // Create an event listener for new guild members
 bot.client.on("guildMemberAdd", (member) => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(
-    (ch) => ch.name === "public-general"
-  );
-  if (!channel) return;
-
-  const embed = new Discord.MessageEmbed()
-    .setTitle(
-      `Welcome, ${member.user.username}, to The Ruffian Posse - Grizzly Hills`
-    )
-    .setAuthor(
-      member.client.user.username,
-      "https://i.pinimg.com/564x/ba/3e/4e/ba3e4e3514a74c8709c55c45b6d850b1.jpg"
-    )
-    .setDescription(
-      `Welcome to The Ruffian Posse Discord Server and Pleasure Palace.\nFeel free to spy on us at Raider.IO and Warcraft Logs.`
-    )
-    .addFields(
-      {
-        name: "Warcraft Logs",
-        value: "https://www.warcraftlogs.com/guild/id/271788",
-        inline: true,
-      },
-      {
-        name: "Raider.IO",
-        value:
-          "https://raider.io/guilds/us/grizzly-hills/The%20Ruffian%20Posse",
-        inline: true,
-      }
-    )
-    .setImage(wowgif)
-    .setTimestamp()
-    .setFooter("Please enjoy your stay, unless you aren't into that.");
-  // Try to set their role to guests and pugs
-  let role = member.guild.roles.cache.find(
-    (role) => role.name === "Guests and Pugs"
-  );
-  role
-    ? member.roles.add(role)
-    : console.log(`Could not add ${role} to ${member.client.user.username}`);
-  channel.send(embed);
+  let embed = greetingEmbed(member);
 });
---
+++ embeds/invite-embed.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+let joinUsGif =
+  "https://media3.giphy.com/media/RMTQiRYAuvvJb1k6al/giphy.gif?cid=ecf05e47adnbygt9d1gqey48popd2s2yj003aoo2wm7fv4c2&rid=giphy.gif";
+
+function inviteEmbed(msg, invite) {
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(msg.client.user.username)
+    .setTitle(`Your invite, ${msg.author.tag}`)
+    .setDescription(invite)
+    .setImage(joinUsGif);
+  msg.reply(embed);
+}
+
+module.exports = inviteEmbed;
