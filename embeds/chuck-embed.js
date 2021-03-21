218c5212c4ef5e9e8b94f9d5cec8be544158af7c
Jason Shive <jwshive@gmail.com>
2021-03-21 08:21:42 -0400

    Changed the title and description fields around

--- embeds/chuck-embed.js
+++ embeds/chuck-embed.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 function keysEmbed(msg, data) {
   const embed = new Discord.MessageEmbed()
     .setColor("#0099ff")
-    .setTitle("Don't Fuck with Chuck!")
+    .setDescription("Don't Fuck with Chuck!")
     .setAuthor(
       "Chuck Norris",
       "https://images02.military.com/sites/default/files/styles/full/public/media/veteran-jobs/content-images/2016/03/chucknorris.jpg?itok=_J3M4O-S"
     )
-    .setDescription(data.value)
+    .setTitle(data.value)
     .setImage(
       "https://i.pinimg.com/originals/b4/d0/bc/b4d0bc7a0a9a9d6f34274e7be5eabfe1.gif"
     )
