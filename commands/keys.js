d829fdacecd79eb99c483f1f47531c3db5930968
Jason Shive <jwshive@gmail.com>
2021-03-23 12:44:56 -0400

    Attempting to catch some errors

--- commands/keys.js
+++ commands/keys.js
@@ -10,6 +10,7 @@ module.exports = {
     msg.delete();
     if (msg.content.length <= 6) {
       Key.find((err, result) => {
+        if (err) throw err;
         let embed = keysEmbed(msg, result);
       });
     } else {
@@ -35,7 +36,7 @@ module.exports = {
               msg.reply("New Key Added.");
             })
             .catch((err) => {
-              console.log(err.code);
+              console.log(`Error caught in keys.js command: ${err.code}`);
               if (err.code === 11000) {
                 msg.reply("You've already recorded your key.");
               }
@@ -50,7 +51,7 @@ module.exports = {
             key_level: key_level,
             key_instance: key_instance,
           };
-          Key.findOneAndUpdate(filter, update, { new: true }, (result) => {
+          Key.findOneAndUpdate(filter, update, { new: true }, () => {
             msg.reply("I've updated your key, thanks!");
           });
         }
--- embeds/blame-embed.js
+++ embeds/blame-embed.js
@@ -1,17 +1,21 @@
 const Discord = require("discord.js");
 
 function blameEmbed(msg, blame, author) {
-  const embed = new Discord.MessageEmbed()
-    .setColor("#0099ff")
-    .setAuthor(
-      `Artisanal Failure Excuses - Crafted for ${msg.member.displayName}.`,
-      "https://i.pinimg.com/564x/ba/3e/4e/ba3e4e3514a74c8709c55c45b6d850b1.jpg"
-    )
-    .setTitle(`:bell: ${blame} :bell: --${author}`)
-    .setImage("https://media2.giphy.com/media/vX9WcCiWwUF7G/200.gif")
-    .setTimestamp()
-    .setFooter("Hey buddy, next time, just don't be so bad.");
-  msg.channel.send(embed);
+  try {
+    const embed = new Discord.MessageEmbed()
+      .setColor("#0099ff")
+      .setAuthor(
+        `Artisanal Failure Excuses - Crafted for ${msg.member.displayName}.`,
+        "https://i.pinimg.com/564x/ba/3e/4e/ba3e4e3514a74c8709c55c45b6d850b1.jpg"
+      )
+      .setTitle(`:bell: ${blame} :bell: --${author}`)
+      .setImage("https://media2.giphy.com/media/vX9WcCiWwUF7G/200.gif")
+      .setTimestamp()
+      .setFooter("Hey buddy, next time, just don't be so bad.");
+    msg.channel.send(embed);
+  } catch (err) {
+    console.log(`Error on blame embed: ${err}`);
+  }
 }
 
 module.exports = blameEmbed;
