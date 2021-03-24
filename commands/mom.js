17482ae43c89fa602eee0f44e3db2d4580af5588
Jason Shive <jwshive@gmail.com>
2021-03-23 21:43:35 -0400

    quick yo momma joke

---
+++ commands/mom.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+const jokeEmbed = require("../embeds/joke-embed");
+const getMommaJoke = require("../apis/apiRequest");
+const url = "https://api.yomomma.info/";
+
+module.exports = {
+  text: "!mom",
+  description: "Random Yo Momma Joke",
+  usage: "!joke - Get hit with the best Yo Momma you've ever heard.",
+  callback: (msg) => {
+    const mommaJoke = getMommaJoke(url)
+      .then((data) => {
+        let embed = jokeEmbed(msg, data);
+      })
+      .catch((err) => console.log(err));
+  },
+};
