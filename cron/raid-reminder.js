8cf99b5112c5a1848a9280e5e4f44a5c89ffe8ba
Jason Shive <jwshive@gmail.com>
2021-09-28 06:11:48 -0400

    Removed Friday raid reminder.

--- cron/raid-reminder.js
+++ cron/raid-reminder.js
@@ -2,7 +2,7 @@ const cron = require("node-cron");
 const raidReminderEmbed = require("../embeds/raid-reminder-embed");
 
 function sendReminder(bot) {
-  cron.schedule("0 19 * * tuesday,thursday,friday", () => {
+  cron.schedule("0 19 * * tuesday,thursday", () => {
     let channel = bot.client.channels.cache.find(
       (channel) => channel.name === "guild-general"
     );
