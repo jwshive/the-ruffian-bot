29a8da464462eca7a9bfbf955b72c14502c2682d
Jason Shive <jwshive@gmail.com>
2022-07-05 21:46:34 +0000

    Fixed error in file name

--- commands/woo.js
+++ commands/woo.js
@@ -2,7 +2,7 @@ const playAudio = require("../utils/play_audio");
 
 module.exports = {
   text: "!woo",
-  description: "Gimmie 2 claps and a Ric Flair!?",
+  description: "Gimmie 2 claps and a Ric Flair!",
   usage: "!woo - The Nature Boy.",
   callback: (msg) => {
     if (!msg.member.voice.channel) {
@@ -10,7 +10,7 @@ module.exports = {
       console.log("User was not in a voice channel.");
     } else {
       // Let's try to join the channel and play the sound.
-      playAudio(msg, "ric-flair.mp3").catch((err, msg) =>
+      playAudio(msg, "ric_flair.mp3").catch((err, msg) =>
         msg.author.send("I can't write to that channel, try in #guild-general.")
       );
     }
