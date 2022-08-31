62b0bd7199083628a042ab527106397dfa170630
Jason Shive <jwshive@gmail.com>
2022-08-31 06:06:23 -0400

    Added some onyxia wipe sounds

--- commands/dkp.js
+++ commands/dkp.js
@@ -1,16 +1,16 @@
 const playAudio = require("../utils/play_audio");
 
 module.exports = {
-  text: "!oof",
-  description: "It was at this moment ...",
-  usage: "!oof - He Fucked Up.",
+  text: "!dkp",
+  description: "50 DKP MINUS",
+  usage: "!dkp - Watch the tail",
   callback: (msg) => {
     if (!msg.member.voice.channel) {
       msg.reply("Join a voice channel for the full shame experience.");
       console.log("User was not in a voice channel.");
     } else {
       // Let's try to join the channel and play the sound.
-      playAudio(msg, "he_fd_up.mp3").catch((err, msg) =>
+      playAudio(msg, "dkp.mp3").catch((err, msg) =>
         msg.author.send("I can't write to that channel, try in #guild-general.")
       );
     }
