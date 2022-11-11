b17d782c311634124d1d8a0af341c2f2ed20ae6c
Jason Shive <jwshive@gmail.com>
2022-11-10 20:05:43 -0500

    Well no shit

---
+++ commands/noshit.js
@@ -0,0 +1,18 @@
+const playAudio = require("../utils/play_audio");
+
+module.exports = {
+  text: "!noshit",
+  description: "Well No Shit",
+  usage: "!noshit - Shit?",
+  callback: (msg) => {
+    if (!msg.member.voice.channel) {
+      msg.reply("Join a voice channel for the full shame experience.");
+      console.log("User was not in a voice channel.");
+    } else {
+      // Let's try to join the channel and play the sound.
+      playAudio(msg, "well_no_shit.mp3").catch((err, msg) =>
+        msg.author.send("I can't write to that channel, try in #guild-general.")
+      );
+    }
+  },
+};
