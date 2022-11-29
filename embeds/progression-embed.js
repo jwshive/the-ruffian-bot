c8e6fa897d355d9e08ecad97d26ceb6aebeb432a
Jason Shive <jwshive@gmail.com>
2022-11-28 19:46:13 -0500

    commented out some unused values

--- embeds/progression-embed.js
+++ embeds/progression-embed.js
@@ -12,126 +12,126 @@ function progressionEmbed(msg, data) {
     .addFields(
       {
         name: "Fated Sepulcher of the First Ones Progression",
-        value: data.raid_progression["fated-sepulcher-of-the-first-ones"].summary,
+        #value: data.raid_progression["fated-sepulcher-of-the-first-ones"].summary,
       },
       {
         name: "Normal",
-        value: `${data.raid_progression["fated-sepulcher-of-the-first-ones"].normal_bosses_killed} / 11`,
+        #value: `${data.raid_progression["fated-sepulcher-of-the-first-ones"].normal_bosses_killed} / 11`,
         inline: true,
       },
       {
         name: "Heroic",
-        value: `${data.raid_progression["fated-sepulcher-of-the-first-ones"].heroic_bosses_killed} / 11`,
+        #value: `${data.raid_progression["fated-sepulcher-of-the-first-ones"].heroic_bosses_killed} / 11`,
         inline: true,
       },
       {
         name: "Mythic",
-        value: `${data.raid_progression["fated-sepulcher-of-the-first-ones"].mythic_bosses_killed} / 11`,
+        #value: `${data.raid_progression["fated-sepulcher-of-the-first-ones"].mythic_bosses_killed} / 11`,
         inline: true,
       }
     )
     .addFields(
       {
         name: "Fated Sanctum of Domination Progression",
-        value: data.raid_progression["fated-sanctum-of-domination"].summary
+        #value: data.raid_progression["fated-sanctum-of-domination"].summary
       },
       {
         name: "Normal",
-        value: `${data.raid_progression["fated-sanctum-of-domination"].normal_bosses_killed} / 10`,
+        #value: `${data.raid_progression["fated-sanctum-of-domination"].normal_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Heroic",
-        value: `${data.raid_progression["fated-sanctum-of-domination"].heroic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["fated-sanctum-of-domination"].heroic_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Mythic",
-        value: `${data.raid_progression["fated-sanctum-of-domination"].mythic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["fated-sanctum-of-domination"].mythic_bosses_killed} / 10`,
         inline: true,
       }
     )
     .addFields(
       {
         name: "Fated Castle Nathria Progression",
-        value: data.raid_progression["fated-castle-nathria"].summary,
+        #value: data.raid_progression["fated-castle-nathria"].summary,
       },
       {
         name: "Normal",
-        value: `${data.raid_progression["fated-castle-nathria"].normal_bosses_killed} / 10`,
+        #value: `${data.raid_progression["fated-castle-nathria"].normal_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Heroic",
-        value: `${data.raid_progression["fated-castle-nathria"].heroic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["fated-castle-nathria"].heroic_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Mythic",
-        value: `${data.raid_progression["fated-castle-nathria"].mythic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["fated-castle-nathria"].mythic_bosses_killed} / 10`,
         inline: true,
       }
     )
     .addFields(
       {
         name: "Sepulcher of the First Ones Progression",
-        value: data.raid_progression["sepulcher-of-the-first-ones"].summary,
+        #value: data.raid_progression["sepulcher-of-the-first-ones"].summary,
       },
       {
         name: "Normal",
-        value: `${data.raid_progression["sepulcher-of-the-first-ones"].normal_bosses_killed} / 11`,
+        #value: `${data.raid_progression["sepulcher-of-the-first-ones"].normal_bosses_killed} / 11`,
         inline: true,
       },
       {
         name: "Heroic",
-        value: `${data.raid_progression["sepulcher-of-the-first-ones"].heroic_bosses_killed} / 11`,
+        #value: `${data.raid_progression["sepulcher-of-the-first-ones"].heroic_bosses_killed} / 11`,
         inline: true,
       },
       {
         name: "Mythic",
-        value: `${data.raid_progression["sepulcher-of-the-first-ones"].mythic_bosses_killed} / 11`,
+        #value: `${data.raid_progression["sepulcher-of-the-first-ones"].mythic_bosses_killed} / 11`,
         inline: true,
       }
     )
     .addFields(
       {
         name: "Sanctum of Domination Progression",
-        value: data.raid_progression["sanctum-of-domination"].summary
+        #value: data.raid_progression["sanctum-of-domination"].summary
       },
       {
         name: "Normal",
-        value: `${data.raid_progression["sanctum-of-domination"].normal_bosses_killed} / 10`,
+        #value: `${data.raid_progression["sanctum-of-domination"].normal_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Heroic",
-        value: `${data.raid_progression["sanctum-of-domination"].heroic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["sanctum-of-domination"].heroic_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Mythic",
-        value: `${data.raid_progression["sanctum-of-domination"].mythic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["sanctum-of-domination"].mythic_bosses_killed} / 10`,
         inline: true,
       }      
     )
     .addFields(
       {
         name: "Castle Nathria Progression",
-        value: data.raid_progression["castle-nathria"].summary,
+        #value: data.raid_progression["castle-nathria"].summary,
       },
       {
         name: "Normal",
-        value: `${data.raid_progression["castle-nathria"].normal_bosses_killed} / 10`,
+        #value: `${data.raid_progression["castle-nathria"].normal_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Heroic",
-        value: `${data.raid_progression["castle-nathria"].heroic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["castle-nathria"].heroic_bosses_killed} / 10`,
         inline: true,
       },
       {
         name: "Mythic",
-        value: `${data.raid_progression["castle-nathria"].mythic_bosses_killed} / 10`,
+        #value: `${data.raid_progression["castle-nathria"].mythic_bosses_killed} / 10`,
         inline: true,
       }
     )
