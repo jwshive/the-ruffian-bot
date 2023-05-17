030b1512f36a0d95c8dd72256d6231268c457023
Jason Shive <jwshive@gmail.com>
2023-05-16 21:05:32 -0400

    Removed quick chat bot

---
+++ DiscordMessageHandler.js
@@ -0,0 +1,81 @@
+const fs = require('fs');
+const ChatCommand = require('./ChatCommand');
+module.exports = class DiscordMessageHandler {
+    commands = {};
+    constructor({
+        commandsDir,
+        testChannel,
+        testMode = false,
+        ignoreChannels = [],
+        ignoreBots = true,
+        showLiveMessages = true,
+    }) {
+        if (!commandsDir) {
+            throw new Error(
+                'Invalid parameters. You must include a commands directory to create a DiscordMessageHandler'
+            );
+        }
+        if (ignoreChannels && !Array.isArray(ignoreChannels)) {
+            throw new Error(
+                'Invalid parameters. ignoreChannels must be an array.'
+            );
+        }
+        this.testChannel = testChannel;
+        this.testMode = testMode;
+        this.ignoreChannels = ignoreChannels;
+        this.ignoreBots = ignoreBots;
+        this.showLiveMessages = showLiveMessages;
+        try {
+            const commandFiles = fs.readdirSync(commandsDir);
+            commandFiles
+                .filter((commandFile) => commandFile.endsWith('.js'))
+                .forEach((commandFile) => {
+                    const commandConfig = require(`${commandsDir}/${commandFile}`);
+                    commandConfig.text =
+                        commandConfig.text || `!${commandFile.slice(0, -3)}`;
+                    try {
+                        const chatCommand = new ChatCommand(commandConfig);
+                        this.commands[chatCommand.text] = chatCommand;
+                    } catch (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+            console.log(`Registered commands: ${Object.keys(this.commands)}`);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    handleMessage = async (msg) => {
+        //In test mode, only respond to messages in the test channel
+        const channel = msg.channel.name;
+        if (
+            this.testMode &&
+            this.testChannel !== undefined &&
+            channel !== this.testChannel
+        ) {
+            return console.log(
+                `Ignoring message in test mode since it's not in the test channel`
+            );
+        }
+
+        if (this.ignoreChannels && this.ignoreChannels.includes(channel)) {
+            return console.log(
+                `Ignoring message in channel ${channel} based on bot configuration.`
+            );
+        }
+
+        this.showLiveMessages &&
+            console.info(`${msg.author.username}: ${msg.content}`);
+
+        if (msg.author.bot && this.ignoreBots) return;
+        const username = msg.author.username;
+        const parts = msg.content.split(' ');
+
+        const command = parts[0];
+        if (this.commands[command]) {
+            this.commands[command].tryUseCommand(username, msg);
+        }
+    };
+};
--- TheRuffian.js
+++ TheRuffian.js
@@ -1,31 +1,43 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 const greetingEmbed = require("./embeds/greeting-embed");
-const { QuickDiscordBot } = require("quick-chat-bot");
+const DiscordMessageHandler = require('./DiscordMessageHandler');
 const path = require("path");
 const mongoose = require("mongoose");
+require('dotenv').config();
 let bot_token = process.env.BOT_TOKEN;
-
-// Setup the Bot
-const bot = new QuickDiscordBot({
-  botToken: bot_token,
-  commandsDir: path.join(__dirname, "commands"),
-  ignoreChannels: [],
-  testMode: false,
-  testChannel: "public-general",
-  ignoreBots: true,
-});
+const bot = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 
 // Start the bot
-bot.connect();
+const messageHandler = new DiscordMessageHandler({ commandsDir: path.join(__dirname, "commands"), testChannel: "public-general", testMode: false, botToken: bot_token, ignoreChannels: [], showLiveMessages: true });
+
+bot.on('ready', () => {
+    console.log('Connected to Discord');
+    if (this.testMode && this.testChannel) {
+        console.log(
+            `***RUNNING IN TEST MODE*** and only listening to channel ${this.config.testChannel}`
+        );
+    }
+    if (this.ignoreChannels) {
+        console.log(
+            `Ignoring the following channels`,
+            this.config.ignoreChannels
+        );
+    }
+
+    if (!this.showLiveMessages) {
+        console.log(`Live server messages will be suppressed.`);
+    }
+});
+bot.on('message', messageHandler.handleMessage);
+bot.login(bot_token);
 
 // Get this bread
-bot.client.on("ready", () => {
-  bot.client.user.setActivity("!help", { type: "LISTENING" });
+bot.on("ready", () => {
+  bot.user.setActivity("!help", { type: "LISTENING" });
 });
 
 // Create an event listener for new guild members
-bot.client.on("guildMemberAdd", (member) => {
+bot.on("guildMemberAdd", (member) => {
   const logGuest = require("./utils/log_new_member")(member);
   const url = `${process.env.RAIDERIO_URL}&fields=raid_progression`;
   const getProgression = require("./apis/apiRequest");
